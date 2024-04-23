8703b9c4cc794700f0b5916686983411762171e7
StaticPotato <sedin.mesicc@gmail.com>
2024-04-23 19:33:17 +0200

    VS C# and First web read
    Created a clean VS C# project with htmlAgilityPack. Added functionality to read and save first site

---
+++ WebScraper/WebScraper/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.Net.Http;
+using System;
+using System.Threading.Tasks;
+using HtmlAgilityPack;
+using WebScraper;
+
+namespace WebScraper
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            String url = "https://books.toscrape.com/";
+
+            WebScraper scraper = new WebScraper();
+
+            try
+            {
+                scraper.Scrape(url);
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("ERROR: Could not scrape");
+                throw;
+            }
+
+            
+        }
+
+    }
+}
---
+++ WebScraper/WebScraper/WebScraper.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using HtmlAgilityPack;
+
+namespace WebScraper
+{
+    class WebScraper
+    {
+        public void Scrape(String url)
+        {
+            HttpClient client = new HttpClient();
+            HtmlDocument document = new HtmlDocument();
+
+            String html = client.GetStringAsync(url).Result;
+
+            document.LoadHtml(html);
+
+            string path = Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + "/WEBSCRAPE";
+
+            if (!Directory.Exists(path))
+            {
+                Directory.CreateDirectory(path);
+            }
+
+            document.Save(path + "/web.html");
+        }
+    }
+}
