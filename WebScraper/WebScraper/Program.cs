bcf76d7e1e41b480ead86b2f5c72816086d2ba72
StaticPotato <sedin.mesicc@gmail.com>
2024-04-24 00:53:03 +0200

    Added scraping of various files like html, ico, css

--- WebScraper/WebScraper/Program.cs
+++ WebScraper/WebScraper/Program.cs
@@ -8,22 +8,23 @@ namespace WebScraper
 {
     internal class Program
     {
-        static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
             String url = "https://books.toscrape.com/";
 
             WebScraper scraper = new WebScraper();
-
+            string path = Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + "/WEBSCRAPE";
+            scraper.Init(path, url);
             try
             {
-                scraper.Scrape(url);
+                await scraper.Scrape(url);
             }
             catch (Exception)
             {
                 Console.WriteLine("ERROR: Could not scrape");
                 throw;
             }
-
+            Console.WriteLine("Finished");
             
         }
 
--- WebScraper/WebScraper/WebScraper.cs
+++ WebScraper/WebScraper/WebScraper.cs
@@ -8,23 +8,78 @@ namespace WebScraper
 {
     class WebScraper
     {
-        public void Scrape(String url)
-        {
-            HttpClient client = new HttpClient();
-            HtmlDocument document = new HtmlDocument();
+        string m_RootPath = "";
+        string m_RootUrl = "";
+        HttpClient m_Client = new HttpClient();
 
-            String html = client.GetStringAsync(url).Result;
+        public void Init(string rootDirectory, string rootUrl)
+        {
+            m_RootPath = rootDirectory;
+            m_RootUrl = rootUrl;
+        }
+
+        async public Task Scrape(String url)
+        {
+            Console.WriteLine(url);
+            String html = m_Client.GetStringAsync(url).Result;
+
+            HtmlDocument document = new HtmlDocument();
 
             document.LoadHtml(html);
 
-            string path = Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + "/WEBSCRAPE";
+            var links = document.DocumentNode.SelectNodes("//@href");
+
+            if (links != null)
+            {
+                foreach (HtmlNode link in links)
+                {
+                    string linkUrl = link.Attributes["href"].Value;
+
+                    Uri baseUri = new Uri(url);
+
+                    if (!Uri.IsWellFormedUriString(linkUrl, UriKind.Absolute))
+                    {
+                        Uri absoluteUri = new Uri(baseUri, linkUrl);
+                        linkUrl = absoluteUri.ToString();
+                    }
+
+                    string[] splitUrl = linkUrl.Split(m_RootUrl);
+
+                    if (splitUrl.Length > 1 && !File.Exists(m_RootPath + "/" + splitUrl[1]))
+                    {
+                        await ParseLink(splitUrl[1]);
+                        if (linkUrl.Contains(".html"))
+                        {
+                            await Scrape(linkUrl);
+                        }
+                    }
+                }
+            }
+        }
+
+        async public Task ParseLink(String url)
+        {
+            String path = m_RootPath + "/" + url;
+            string outputPath = path;
+            path = path.Substring(0, path.LastIndexOf("/"));
+
+            String rootUrl = m_RootUrl + url;
 
             if (!Directory.Exists(path))
             {
                 Directory.CreateDirectory(path);
+
             }
 
-            document.Save(path + "/web.html");
+            String html = m_Client.GetStringAsync(rootUrl).Result;
+
+            if (!File.Exists(outputPath))
+            {
+                using (StreamWriter outputFile = new StreamWriter(outputPath))
+                {
+                    await outputFile.WriteAsync(html);
+                }
+            }
         }
     }
 }
